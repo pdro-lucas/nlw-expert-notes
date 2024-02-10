ab812450bf7e7ba86b31a5939cbd457c327f309a
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-10 16:18:42 -0300

    feat: add card components

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
+import Logo from '@/assets/logo.svg'
+
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
-  return <h1 className="text-6xl ">Hello World</h1>
+  return (
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
+      <img src={Logo} alt="Logo of NLW EXPERT" className="w-32" />
+      <form>
+        <input
+          type="text"
+          placeholder="Busque suas notas"
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500 focus:placeholder:text-slate-700"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid auto-rows-[250px] grid-cols-3 gap-6">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  )
 }
