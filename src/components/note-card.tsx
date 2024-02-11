310334f569b91c59a390b846659cb35704306ac3
pdro-lucas <pdrolucas.dev@gmail.com>
2024-02-11 01:26:05 -0300

    refactor: update date format with date fns

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,7 +15,10 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="relative flex flex-col gap-3 overflow-hidden rounded-md bg-slate-800 p-5 text-left outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-500">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
